1e40725f39087deebc0cf7d8b25247d2abc073e4
Aleksei Pushkin <jewelstd@gmail.com>
2018-12-10 19:23:54 +1300

    1. Initial check-in.

---
+++ serverless-redis/handler.js
@@ -0,0 +1,107 @@
+const uuid = require('uuid/v4');
+
+const AWS = require('aws-sdk');
+
+const elasticachePoller = require('./poller');
+
+const sns = new AWS.SNS();
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+elasticachePoller.configure(process.env.REDIS_CLUSTER_URI, process.env.REDIS_CLUSTER_PORT, 64, 32);
+
+// const checkReviewStatus = eventStatus => (console.log(JSON.stringify({ eventStatus })), eventStatus && eventStatus.review_saved && (eventStatus.review_saved.status === 'ok' || eventStatus.review_saved.status === 'error'));
+
+const checkReviewStatus = eventStatus => (eventStatus && eventStatus.review_saved && (eventStatus.review_saved.status === 'ok' || eventStatus.review_saved.status === 'error'));
+
+const getOkResponse = review => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    message: `Thank you ${review.author}, your review has been succesfully registered!`,
+    review,
+  }),
+});
+
+const addReview = async ({ body }) => {
+  const { author, reviewText } = JSON.parse(body);
+  console.log({ author, reviewText });
+  const review = {
+    author,
+    reviewText,
+  };
+
+  const correlationId = uuid();
+  const eventId = uuid();
+
+  const snsEvent = {
+    correlationId,
+    eventId,
+    eventType: 'add_review',
+    body: review,
+  };
+
+  const reviewSnsParams = {
+    TopicArn: process.env.REVIEW_SNS_TOPIC_ARN,
+    Message: JSON.stringify(snsEvent),
+  };
+
+  try {
+    await sns.publish(reviewSnsParams).promise();
+    const { object } = await elasticachePoller.pollValue(correlationId, 'review_saved', checkReviewStatus);
+    const { review_saved: { status, review, error } } = JSON.parse(object);
+    if (status === 'error') throw new Error(error);
+    console.log(JSON.stringify({ status, review, error }));
+    return getOkResponse(review);
+  } catch(e) {
+    console.log(e.toString());
+    return { statusCode: 400, body: JSON.stringify({ error: e.toString() }) };
+  }
+};
+
+saveReview = async ({ Records: [{ Sns: sns },] }) => {
+  console.log(JSON.stringify(sns));
+  const eventData = JSON.parse(sns.Message);
+  const { correlationId, eventType, body } = eventData;
+
+  const review = { ...body, id: uuid(), createdTimestamp: new Date().getTime() };
+
+  try {
+    await dynamodb.put({
+      TableName: process.env.REVIEW_TABLE,
+      Item: review,
+    }).promise();
+  
+    const statusSet = await elasticachePoller.setValue(correlationId, 'review_saved', { review, status: 'ok' });
+    console.log(JSON.stringify({ statusSet }));
+  } catch(e) {
+    console.log(e.toString());
+    await elasticachePoller.setValue(correlationId, 'review_saved', { error: e.toString(), status: 'error' });
+  }
+};
+
+sendThankYouEmail = async ({ Records: [{ Sns: sns },] }) => {
+  console.log(JSON.stringify(sns));
+  const eventData = JSON.parse(sns.Message);
+  const { correlationId, eventType, body } = eventData;
+
+  try {
+    console.log('Sorry, not implemented yet 🤭 ...');
+  
+    const statusSet = await elasticachePoller.setValue(correlationId, 'email_sent', { status: 'ok' });
+    console.log(JSON.stringify({ statusSet }));
+  } catch(e) {
+    console.log(e.toString());
+    await elasticachePoller.setValue(correlationId, 'email_sent', { error: e.toString(), status: 'error' });
+  }
+};
+
+updateUserProfile = async ({ Records: [{ Sns: sns },] }) => {
+  console.log('Sorry, not implemented yet 🤭 ...');
+};
+
+module.exports = {
+  addReview,
+  saveReview,
+  sendThankYouEmail,
+  updateUserProfile,
+};
---
+++ serverless-redis/poller.js
@@ -0,0 +1,104 @@
+const redis = require('redis');
+
+var __host = 'localhost';
+var __port = '6379';
+var __retries = 24;
+var __timeout = 128;
+
+const configure = (host = 'localhost', port = '6379', retries = 24, timeout = 128) => {
+    this.__host = host;
+    this.__port = port;
+    this.__retries = retries;
+    this.__timeout = timeout;
+};
+/**
+ * Polls the status of an operation with certain ID
+ * Cache acts like a map with the following structure:
+ * cache[corellationId][operation] = valueObject
+ * 
+ * @param {string} key A key with which the operation is associated.
+ * @param {string} evaluate A function that tests the valueObject to check if the operation has finished yet.
+ */
+const pollValue = (correlationId, operation, evaluate) => {
+    const key = `${corellationId}|${operation}`;
+    if (key === null || key == undefined) {
+        throw "redis-poll: key is undefined";
+    }
+    return new Promise((resolve, reject) => {
+        try{
+            poll(key, evaluate, resolve);
+        }
+        catch(e){
+            reject(e);
+        }
+    });
+};
+
+const poll = (key, evaluate, callback, attemptsLeft = 0) => {
+    var client = redis.createClient({
+        host: this.__host,
+        port: this.__port,
+    });
+    if (key === null || key == undefined) {
+        client.quit();
+        throw "redis: key is undefined";
+    }
+    setTimeout(() => {
+        client.get(key, (err, result) => {
+            const _o = JSON.parse(result);
+            if (err) throw err;
+            else if (evaluate(_o)) callback({ status: "completed", object: result });
+            else if (attemptsLeft == 1) throw new Error('Exceeded maximum allowed attempts to poll data from the cache.');
+            else poll(key, evaluate, callback, attemptsLeft ? attemptsLeft -1 : this.__retries - 1); // TODO : update and check retries before retrying
+        });
+        client.quit();
+    }, this.__timeout);
+};
+
+/**
+ * Cache acts like a map with the following structure:
+ * cache[corellationId][operation] = valueObject
+ * 
+ * @param {string} corellationId The corellation id of the caller. The key of the cached value.
+ * @param {string} operation  The operation request. The second level key of the cached value.
+ * @param {object} valueObject The cached value associated with the the corellation id and the operation.
+ */
+const setValue = (corellationId, operation, valueObject)  => {
+    return new Promise((resolve, reject) => {
+        try{
+            setRedisStatus(corellationId, operation, valueObject, resolve);
+        }
+        catch(e){
+            reject(e);
+        }
+    });
+};
+
+const setRedisStatus = (corellationId, operation, valueObject, callback) => {
+    var client = redis.createClient({
+        host: this.__host,
+        port: this.__port,
+    });
+    const id = `${corellationId}|${operation}`;
+    client.get(id, (e, result) => {
+        let object = null;
+        if(e) { throw e; client.quit(); }
+        else if (result === null){
+            object = {};
+            object[operation] = valueObject;
+        }
+        else{
+            object = JSON.parse(result);
+            object[operation] = valueObject;
+        }
+        client.set(id, JSON.stringify(object), (error, reply) => {
+            callback({object, error, reply});
+
+        });
+        client.quit();
+    });
+};
+
+module.exports.pollValue = pollValue;
+module.exports.configure = configure;
+module.exports.setValue = setValue;
